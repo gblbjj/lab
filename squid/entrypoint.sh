b98c8c81b03e4b99d718e8e6da19ca755fdcf778
gblbjj <oliveira992gabriel@gmail.com>
2020-02-06 09:16:27 -0300

    start

--- squid/entrypoint.sh
+++ squid/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ ! -d "/nginx" ]; then
-    echo "/nginx dir not exits. Please mount or create it"
-    exit 1
 
-fi
 
-nginx -g "daemon off;"
+
+service squid start 
+
+echo "Starting squid"
